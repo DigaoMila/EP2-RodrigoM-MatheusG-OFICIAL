2aef8f53b3d982a626f908cb7ccdbffc549245d7
DigaoMila <rodrigosm17@al.insper.edu.br>
2026-05-01 23:22:40 -0300

    Programa jogo 3

--- programa.py
+++ programa.py
@@ -1,6 +1,6 @@
 from funcoes import *
 
-cartela = {'regra_simples': {1:-1, 2:-1, 3:-1, 4:-1, 5:-1, 6:-1}, 'regra_avancada': {'sem_combinacao': -1, 'quadra':-1, 'full_house':-1, 'sequencia_baixa':-1-1, 'sequencia_alta':-1, 'cinco_iguais':-1}}
+cartela = {'regra_simples': {1:-1, 2:-1, 3:-1, 4:-1, 5:-1, 6:-1}, 'regra_avancada': {'sem_combinacao': -1, 'quadra':-1, 'full_house':-1, 'sequencia_baixa':-1, 'sequencia_alta':-1, 'cinco_iguais':-1}}
 
 imprime_cartela(cartela)
 
